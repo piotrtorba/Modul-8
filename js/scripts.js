218ca4acd0662182e1f1bc639c2028a43b419a0b
Torba <torba.piotr@gmail.com>
2017-05-06 14:14:20 +0200

    W

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+//Plik scripts.js
+
+var name = prompt('Hej, jak masz na imie ?');
+//To jest pierwszy komentarz  alert('Witaj, ' + name);
+//to jest drugi komentarz
+/* To jest trzeci komentarz 
+console.log('Witaj, ' + name);
+*/
